d6f3e15bf32a16c8f6912373fc7539f09a6b1c37
pankajmasaye88 <pankajmasaye88@gmail.com>
2025-08-15 17:21:34 +0530

    wrong python code and pre commit script

---
+++ Python-Practice/demo.py
@@ -0,0 +1,14 @@
+#wrong code for checking flake8 using pre commit
+
+def my_function():
+    a = 5
+    b = 6
+    c = 6
+    password = "pan@113"
+
+    return d * password
+
+my_function()
+
+
+test()
